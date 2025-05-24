e693ff29656e71ee400a39a37118219b995b8519
Brayan <brayanprse@unisabana.edu.co>
2025-05-24 17:45:38 -0500

    Keep Session Open

--- app/src/main/java/com/example/husermenapp/AuthenticationActivity.kt
+++ app/src/main/java/com/example/husermenapp/AuthenticationActivity.kt
@@ -1,5 +1,6 @@
 package com.example.husermenapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -7,9 +8,12 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.husermenapp.databinding.ActivityAuthenticationBinding
 import com.example.husermenapp.fragments.AuthenticationLoginFragment
 import com.example.husermenapp.utils.FragmentUtils.replaceFragment
+import com.google.firebase.auth.FirebaseAuth
 
 class AuthenticationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAuthenticationBinding
+
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +26,16 @@ class AuthenticationActivity : AppCompatActivity() {
             insets
         }
 
-        replaceFragment(
-            supportFragmentManager,
-            R.id.authFragments,
-            AuthenticationLoginFragment(),
-            false
-        )
+        if (auth.currentUser != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            replaceFragment(
+                supportFragmentManager,
+                R.id.authFragments,
+                AuthenticationLoginFragment(),
+                false
+            )
+        }
     }
 }
